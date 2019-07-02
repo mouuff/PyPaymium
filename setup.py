255c51d4e7ecdcc89e0764498cddc2f1e2ee4142
Arnaud Alies <arnaudalies.py@gmail.com>
2019-07-02 23:57:19 +0200

    created setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+"""Setup generator file."""
+from setuptools import setup, find_packages
+import sys
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+requirements = open("requirements.txt").read().splitlines()
+packages = find_packages()
+# packages.remove("tests")
+
+setup(
+    name='paymium',
+    packages=packages,
+    version='1.0',
+    description='Paymium api',
+    author='mouuff',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author_email='arnaudalies.py@gmail.com',
+    url='https://github.com/mouuff/PyPaymium',
+    keywords=['tool', 'api', 'paymium', 'bot', 'pypaymium'],
+    classifiers=['Intended Audience :: Developers',
+                 'Development Status :: 4 - Beta',
+                 'Programming Language :: Python :: 3 :: Only',
+                 'Topic :: Software Development'
+                 ],
+    install_requires=requirements,
+    include_package_data=True
+)
