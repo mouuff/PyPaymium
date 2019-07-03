cfd6b25fb62628abe5bc78070f54fd4915222920
Arnaud Alies <arnaudalies.py@gmail.com>
2019-07-03 11:44:27 +0200

    Update test_public_api.py

--- tests/test_public_api.py
+++ tests/test_public_api.py
@@ -5,6 +5,24 @@ from .base_test_case import BaseTestCase
 
 
 class TestPublicApi(BaseTestCase):
-    def test_1(self):
+    def test_ticker(self):
         p = paymium.Api()
-        print(p.get_trades(since=time.time()-10000))
+        ticker = p.get_ticker()
+        assert ticker["currency"] == "EUR"
+        assert ticker["bid"] < ticker["ask"]
+        assert ticker["low"] < ticker["high"]
+
+    def test_trades(self):
+        p = paymium.Api()
+        trades = p.get_trades()
+        assert len(trades) > 0
+        first = trades[0]
+        assert 'uuid' in first
+        assert 'currency' in first
+        assert 'traded_btc' in first
+        assert 'traded_currency' in first
+        assert 'created_at' in first
+        assert 'currency' in first
+        assert 'side' in first
+        assert 'price' in first
+        assert 'created_at_int' in first
